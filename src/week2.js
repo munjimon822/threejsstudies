33e709b48293bf80fefaa111afcc0821dec1a743
MinG822 <lucida822@naver.com>
2024-11-05 21:44:24 +0900

    fix week2.js

--- src/week2.js
+++ src/week2.js
@@ -13,11 +13,11 @@ const renderer = new THREE.WebGLRenderer({ canvas: canvasEl });
 
 // create camera
 const fov = 65;
-const aspect = 1;
-const near = 0.1;
-const far = 100;
+const aspect = window.clientWidth/window.clientHeight; // Fixed
+const near = 0.1; // Note : not to be too small like 0.001 or too big like 999 
+const far = 1000;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.set(0, 1.6, 8)
+camera.position.set(0, 1.6, 16)
 
 // create scene
 const scene = new THREE.Scene();
@@ -26,43 +26,60 @@ const scene = new THREE.Scene();
 const color = 0xFFFFFF;
 const intensity = 2;
 const light = new THREE.AmbientLight(color, intensity);
-light.position.set(0, 0, 80);
 scene.add(light);
 
-// stand front wall
-function rotateWall(mesh, x, y, z) {
-  mesh.rotateX(x)
-  mesh.rotateY(y)
-  mesh.rotateZ(z)
-}
-
-const wallInfo = { w: 800, h: 400, x: 0, y: 0, z: -40, rot: [0, 0, 0] }
-const wallMaterial = new THREE.MeshBasicMaterial({ color: 0x5C5A51 });
+const wallInfo = { w: 800, h: 400, x: 0, y: 0, z: -2}
+const wallMaterial = new THREE.MeshPhongMaterial({ color: 0x5C5A51 });
 const wall = new THREE.Mesh(
   new THREE.PlaneGeometry(wallInfo.w, wallInfo.h),
   wallMaterial
 )
 
 wall.position.set(wallInfo.x, wallInfo.y, wallInfo.z)
-rotateWall(wall, ...wallInfo.rot)
 scene.add(wall)
+
+const leftSideWallInfo = { w: 0.3, h: 400,  x:-10, y: 0, z: 0}
+const sideWallMaterial = new THREE.MeshBasicMaterial({ color: 0x5C5A51 });
+
+const leftSideWall = new THREE.Mesh(
+  new THREE.BoxGeometry(leftSideWallInfo.w, 800, leftSideWallInfo.h),
+  sideWallMaterial
+)
+
+leftSideWall.position.set(leftSideWallInfo.x, leftSideWallInfo.y, leftSideWallInfo.z)
+scene.add(leftSideWall)
+
+const rightSideWallInfo = { w: 0.3, h: 400, x: 10, y: 0, z: 0 }
+const rightSideWall = new THREE.Mesh(
+  new THREE.BoxGeometry(rightSideWallInfo.w, 800, rightSideWallInfo.h),
+  sideWallMaterial
+)
+
+rightSideWall.position.set(rightSideWallInfo.x, rightSideWallInfo.y, rightSideWallInfo.z)
+scene.add(rightSideWall)
 
 const floorGeometry = new THREE.BoxGeometry(800, 0.3, 100);
 const floorMaterial = new THREE.MeshPhongMaterial({ color: 0x5C5A51 });
 const floor = new THREE.Mesh(floorGeometry, floorMaterial);
-floor.position.set(0, -1.2, 48)
+floor.position.set(0, -1.2, 10) // Note cause front wall z -40, it should be 10, 다음부터는 기준점을 잘잡기
 scene.add(floor)
+
+const ceilGeometry = new THREE.BoxGeometry(800, 0.3, 100);
+const ceilrMaterial = new THREE.MeshPhongMaterial({ color: 0x5C5A51 });
+const ceil = new THREE.Mesh(ceilGeometry, ceilrMaterial);
+ceil.position.set(0, 6, 10) // Note cause front wall z -40, it should be 10, 다음부터는 기준점을 잘잡기
+scene.add(ceil)
 
 // create standar
 function createStander(x,y,z) {
   const material = new THREE.MeshPhongMaterial({ color: 0xBABABA });
 
   const boxWidth = 1;
-  const boxHeight = 1.2;
+  const boxHeight = 1.6;
   const boxDepth = 1;
   const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
   const cube = new THREE.Mesh(geometry, material);
-  cube.position.set(x, y-0.1, z)
+  cube.position.set(x, y-0.4, z)
   return cube
 }
 
@@ -71,18 +88,16 @@ function createStander(x,y,z) {
 function createPinLight(x, y, z) {
   const color = 0xffd285;
   const intensity = 100;
-  // const light = new THREE.DirectionalLight(color, intensity);
   const light = new THREE.PointLight(color, intensity, 3);
-  light.position.set(x, y, z);
+  light.position.set(x, 0, z-0.1);
   return light
 }
 
-function displayObj(scene, obj, x, z) {
-  const pinLight = createPinLight(x, 4, z)
+function displayObj(scene, x, z) {
+  const pinLight = createPinLight(x, 0, z)
   const stander = createStander(x, 0, z)
   scene.add(pinLight)
   scene.add(stander)
-  pinLight.target = obj
 }
 
 // create first object
@@ -120,7 +135,7 @@ function createHeart() {
 const firstObj = createHeart()
 firstObj.position.set(-4.4, 1.2 , 0.2)
 scene.add(firstObj)
-displayObj(scene, firstObj, -4.4, 0.2)
+displayObj(scene, -4.4, 0.2)
 
 
 // create second object
@@ -143,7 +158,7 @@ function createDonut() {
 const secondObj = createDonut()
 secondObj.position.set(0, 1.2, 0)
 scene.add(secondObj)
-displayObj(scene, secondObj, 0, 0.2)
+displayObj(scene, 0, 0.2)
 
 // creat third object
 function createKnot() {
@@ -165,7 +180,7 @@ function createKnot() {
 const thirdObj = createKnot()
 thirdObj.position.set(4.4, 1.2, 0.2)
 scene.add(thirdObj)
-displayObj(scene, thirdObj, 4.4, 0.2)
+displayObj(scene, 4.4, 0.2)
 
 const objects = [firstObj, secondObj, thirdObj]
 const axesHelper = new THREE.AxesHelper(5);
